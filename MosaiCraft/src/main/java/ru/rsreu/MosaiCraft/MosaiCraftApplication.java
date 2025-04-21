c0c66f2a15188955d0eafa109b483832aa069f9a
Kirill <kirill.rastrygin@gmail.com>
2025-04-21 12:54:56 +0300

    start

---
+++ MosaiCraft/src/main/java/ru/rsreu/MosaiCraft/MosaiCraftApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.MosaiCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaiCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaiCraftApplication.class, args);
+	}
+
+}
---
+++ MosaiCraft/src/test/java/ru/rsreu/MosaiCraft/MosaiCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.MosaiCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosaiCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
